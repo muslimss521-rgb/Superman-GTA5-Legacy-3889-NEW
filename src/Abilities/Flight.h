95243f54d5ec893056fb95de8579466ad94c0997
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-21 21:49:11 +0300

    Update Flight.h

--- src/Abilities/Flight.h
+++ src/Abilities/Flight.h
@@ -1,15 +1,26 @@
 #pragma once
 
-#include "../../include/ScriptHookV/main.h"
-#include "../../include/ScriptHookV/natives.h"
-#include "../../include/ScriptHookV/types.h"
+#include "../../include/ScriptHookV/script.h"
 
 namespace Flight
 {
+    enum class State
+    {
+        Disabled,
+        Takeoff,
+        Flying,
+        Boost,
+        Landing
+    };
+
     void Initialize();
+
     void Enable();
     void Disable();
+
     void Update(Ped playerPed);
 
     bool IsEnabled();
+
+    State GetState();
 }
