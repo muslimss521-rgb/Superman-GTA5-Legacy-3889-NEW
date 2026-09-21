d2a59b7a1bc2fafac5ad1a7f7f7c46e3400f4570
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-21 22:29:56 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,22 @@
+#include <windows.h>
+#include "../include/ScriptHookV/main.h"
+#include "Superman.h"
 
+static Superman g_superman;
+
+void ScriptMain() {
+    while (true) {
+        g_superman.Update();
+        WAIT(0);
+    }
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID) {
+    if (reason == DLL_PROCESS_ATTACH) {
+        DisableThreadLibraryCalls(hModule);
+        scriptRegister(hModule, ScriptMain);
+    } else if (reason == DLL_PROCESS_DETACH) {
+        scriptUnregister(hModule);
+    }
+    return TRUE;
+}
