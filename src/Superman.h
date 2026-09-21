7b5f9e4b567bd6fa8e034c6918c1319174fbda6a
muslimss521-rgb <muslim.ss521@gmail.com>
2026-09-21 22:33:57 +0300

    Update Superman.h

--- src/Superman.h
+++ src/Superman.h
@@ -1,1 +1,19 @@
+#pragma once
+#include "Native.h"
+#include "Flight.h"
+#include "Abilities.h"
 
+class Superman {
+public:
+    void Update();
+private:
+    bool enabled_ = false;
+    bool flight_ = false;
+    bool heat_ = false;
+    bool freeze_ = false;
+    FlightSystem flightSystem_;
+    Abilities abilities_;
+
+    void Toggle();
+    void HandlePowers(int ped);
+};
